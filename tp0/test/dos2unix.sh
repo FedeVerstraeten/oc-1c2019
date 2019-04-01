d0be61058bf0db3551e0e477e4c67c463f28e05a
Federico Verstraeten <federico.verstraeten@gmail.com>
2019-04-01 01:23:29 -0300

    enhancement of the enc-dec auxiliary scripts

--- tp0/test/dos2unix.sh
+++ tp0/test/dos2unix.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
 # Windows-DOS to Unix
-awk '{ sub("\r$", ""); print }' $1 > $2
+OPT_IN="$1"
+OPT_OUT="$3"
+
+if [ "$OPT_IN" == "-i" ]; then
+  INPUT="$2";
+else
+  INPUT="$1";
+fi
+
+if [ "$OPT_OUT" == "-o" ]; then
+  OUTPUT="$4";
+fi
+
+if [[ -z $OUTPUT  ]]; then
+  awk '{ sub("\r$", ""); print }' "$INPUT" 
+else
+  awk '{ sub("\r$", ""); print }' "$INPUT" > "$OUTPUT"
+fi
